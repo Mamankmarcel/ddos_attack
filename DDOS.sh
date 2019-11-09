1338491e8271a7544c1b8c4ac1f16cac729fc278
Mamankmarcel <57529908+Mamankmarcel@users.noreply.github.com>
2019-11-09 12:36:23 +0700

    Add files via upload

---
+++ DDOS.sh
@@ -0,0 +1,26 @@
+
+#!/bin/sh
+clear
+echo "================"
+echo "   DDOS ATTACK  "
+echo "     WEBSITE    "
+echo "================"
+echo "youtube: mamank marcel"
+echo
+echo "script v1.0"
+echo "\033[0;36m"
+sleep 2
+read -p "Alamat ip website: " ip
+read -p "size: " size
+sleep 2
+echo
+echo "mendapatkan alamat ip $ip "
+sleep 1
+echo "mengirim $size ke website"
+echo
+sleep 1
+echo "anda berhasil"
+sleep 1
+echo "DDOS attack bersama kawanmu agar lebih maksimal!! "
+sleep 3
+exit
